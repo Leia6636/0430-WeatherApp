538474dea607f8509fa52e8262b6721abbd1f907
Leia6636 <168165525+Leia6636@users.noreply.github.com>
2024-04-26 14:22:14 +0800

    Update weatherCode.swift

--- weatherCode.swift
+++ weatherCode.swift
@@ -1,13 +1,5 @@
 import ImageIO
 
-extension UIView{
-    func addShadow(shadowOpacity:Float,shadowRadius:CGFloat){
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOpacity = shadowOpacity
-        self.layer.shadowOffset = .zero
-        self.layer.shadowRadius = shadowRadius
-    }
-}
 extension UIImageView {
 
     public func loadGif(name: String) {
