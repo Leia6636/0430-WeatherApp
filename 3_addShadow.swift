3140ce36c588aae4ff2b761bc9b3c11b445f768e
Leia6636 <168165525+Leia6636@users.noreply.github.com>
2024-04-28 23:05:34 +0800

    Delete 3_addShadow.swift

--- 3_addShadow.swift
+++
@@ -1,8 +0,0 @@
-extension UIView{
-    func addShadow(shadowOpacity:Float,shadowRadius:CGFloat){
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOpacity = shadowOpacity
-        self.layer.shadowOffset = .zero
-        self.layer.shadowRadius = shadowRadius
-    }
-}
